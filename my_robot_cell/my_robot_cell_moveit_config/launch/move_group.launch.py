6d69303db012be460fb58088b70758e74ab041cb
KTeddMo <kamomoyojpn@gmail.com>
2026-07-14 21:48:20 +0900

    Update Humble robot cell tutorial for dual manipulators

--- my_robot_cell/my_robot_cell_moveit_config/launch/move_group.launch.py
+++ my_robot_cell/my_robot_cell_moveit_config/launch/move_group.launch.py
@@ -1,9 +1,59 @@
 from moveit_configs_utils import MoveItConfigsBuilder
+from launch import LaunchDescription
+from launch_ros.actions import Node
 from moveit_configs_utils.launches import generate_move_group_launch
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
     moveit_config = MoveItConfigsBuilder(
         "my_robot_cell", package_name="my_robot_cell_moveit_config"
     ).to_moveit_configs()
-    return generate_move_group_launch(moveit_config)
+    move_group_configuration = {
+        "publish_robot_description_semantic": True,
+        "allow_trajectory_execution": True,
+        "publish_planning_scene": True,
+        "publish_geometry_updates": True,
+        "publish_state_updates": True,
+        "publish_transforms_updates": True,
+    }
+
+    move_group_params = [
+        moveit_config.to_dict(),
+        move_group_configuration,
+    ]
+
+    move_group_node = Node(
+        package="moveit_ros_move_group",
+        executable="move_group",
+        output="screen",
+        parameters=move_group_params,
+        additional_env={"DISPLAY": ":0"},
+    )
+
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare("my_robot_cell_moveit_config"), "config", "moveit.rviz"]
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2_moveit",
+        output="log",
+        arguments=["-d", rviz_config_file],
+        parameters=[
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.robot_description_kinematics,
+            moveit_config.planning_pipelines,
+            moveit_config.joint_limits,
+        ],
+    )
+
+    return LaunchDescription(
+        [
+            move_group_node,
+            rviz_node,
+        ]
+    )
